76505fc28b04d745c1ab61b2d481f50df89617c6
ramazan351 <ramazan351.ra@gmail.com>
2021-07-09 12:54:59 +0300

    datetime null safety issue fixed

--- lib/widgets/weather_image.dart
+++ lib/widgets/weather_image.dart
@@ -1,8 +1,18 @@
+import 'package:bloc_weather/blocs_weather/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 class MyWeatherImageWidget extends StatelessWidget {
-  const MyWeatherImageWidget({Key? key}):super(key: key);
+  const MyWeatherImageWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const FlutterLogo(size: 144,);
+    Bloc _bloc = BlocProvider.of<WeatherBloc>(context);
+    return BlocBuilder(
+        bloc: _bloc,
+        builder: (ctx,state) {
+
+          return Image.network("https://www.metaweather.com/static/img/weather/png/${(state as WeatherLoadedState).weatherModel!.consolidatedWeather?[0].weatherStateAbbr}.png",width: MediaQuery.of(context).size.width*0.8,height: MediaQuery.of(context).size.height*0.3,);
+        });
   }
 }
